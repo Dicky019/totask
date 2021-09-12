b5079a0dd8a8e51daf17f801f4909c2e4cd331e9
DickyDarmawan <dicky93darmawan@gmail.com>
2021-09-12 23:53:29 +0800

    finis update

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'app/routes/app_pages.dart';
+import 'app/data/utils/assets.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  final user = Rx(FirebaseAuth.instance.currentUser);
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
+      title: "Totask",
+      initialRoute: (user.value == null) ? Routes.AUTH : AppPages.INITIAL,
       getPages: AppPages.routes,
+      defaultTransition: Transition.fade,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(),
+        primaryColor: primer,
+        primarySwatch: Colors.purple,
+      ),
     ),
   );
 }
